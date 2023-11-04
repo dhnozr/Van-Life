f5e00eea6c325bd9d12e9cf7248cfaf176617bf0
Duhan Ozarslan <r.d.ozarslan@gmail.com>
2023-11-04 14:47:44 +0300

    footer imported and added to layout

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Navbar from './components/Navbar';
+import Footer from './components/Footer';
 import { Link, Outlet } from 'react-router-dom';
 
 export default function Layout() {
@@ -7,6 +8,7 @@ export default function Layout() {
     <>
       <Navbar />
       <Outlet />
+      <Footer />
     </>
   );
 }
