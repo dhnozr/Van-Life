cce4f043ca1cbaea9402465c45cd82a0cde0846e
Duhan Ozarslan <r.d.ozarslan@gmail.com>
2023-11-04 11:04:44 +0300

    navbar styles updated

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,17 +1,16 @@
 import React from 'react';
 import logo from '../images/logog.png';
-import './Navbar.css';
 
 import { Link } from 'react-router-dom';
 
 export default function Navbar() {
   return (
-    <header className="flex justify-between">
+    <header className="flex justify-between items-center bg-[#FFF7ED] h-16 p-4">
       <div>
         <img width={150} src={logo} alt="" />
       </div>
       <nav>
-        <ul>
+        <ul className="flex gap-3 text-lg">
           <li>
             <Link>About</Link>
           </li>
