693dfb6b4a041394548df0d07cf092781dba33c2
Duhan Ozarslan <r.d.ozarslan@gmail.com>
2023-11-04 11:14:19 +0300

    about,home,vans imported and routes are adjusted

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,21 @@
 import { useState } from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import About from './pages/About';
 import Layout from './Layout';
+import Home from './pages/Home';
+import Vans from './pages/Vans';
 import './App.css';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Layout />}></Route>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="about" element={<About />} />
+          <Route path="vans" element={<Vans />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
