b795ac3bc108395dbd383b94ba91401bca4c77f0
Duhan Ozarslan <r.d.ozarslan@gmail.com>
2023-11-04 11:15:00 +0300

    outlet imported

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import Navbar from './components/Navbar';
-import { Link } from 'react-router-dom';
+import { Link, Outlet } from 'react-router-dom';
 
 export default function Layout() {
   return (
     <>
       <Navbar />
+      <Outlet />
     </>
   );
 }
