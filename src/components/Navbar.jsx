8775cf85571584652c7dd151ad6b25de172e188b
Duhan Ozarslan <r.d.ozarslan@gmail.com>
2023-11-04 11:14:37 +0300

    links to about and vans added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,15 +7,17 @@ export default function Navbar() {
   return (
     <header className="flex justify-between items-center bg-[#FFF7ED] h-16 p-4">
       <div>
-        <img width={150} src={logo} alt="" />
+        <Link to={`/`}>
+          <img width={150} src={logo} alt="" />
+        </Link>
       </div>
       <nav>
         <ul className="flex gap-3 text-lg">
           <li>
-            <Link>About</Link>
+            <Link to={`about`}>About</Link>
           </li>
           <li>
-            <Link>Vans</Link>
+            <Link to={`vans`}>Vans</Link>
           </li>
         </ul>
       </nav>
