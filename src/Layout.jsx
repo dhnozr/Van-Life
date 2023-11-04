06e863e949ea4f54e6bd609cd361bb8db3543e93
Duhan Ozarslan <r.d.ozarslan@gmail.com>
2023-11-04 10:21:53 +0300

    layout created

---
+++ src/Layout.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import Navbar from './components/Navbar';
+import { Link } from 'react-router-dom';
+
+export default function Layout() {
+  return (
+    <>
+      <Navbar />
+    </>
+  );
+}
