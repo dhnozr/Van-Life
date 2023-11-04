de0c141b70da818978e73dff610ef974bb8b1ee1
Duhan Ozarslan <r.d.ozarslan@gmail.com>
2023-11-04 10:04:58 +0300

    react-router-dom installed

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
-
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
 
 function App() {
