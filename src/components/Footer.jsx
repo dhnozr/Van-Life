cf2d29ddd2b17156c0187fedeade34f8f02936a6
Duhan Ozarslan <r.d.ozarslan@gmail.com>
2023-11-05 13:28:59 +0300

    footer  text changed

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 export default function Footer() {
   return (
     <div className="relative h-10 bottom-0">
-      <footer className="bg-[#252525] absolute flex items-center justify-center text-xl text-white left-0 right-0 bottom-0 top-0">
+      <footer className="bg-[#252525]  absolute flex items-center justify-center text-sm text-white left-0 right-0 bottom-0 top-0">
         <p>Ⓒ 2022 #VANLIFE</p>
       </footer>
     </div>
