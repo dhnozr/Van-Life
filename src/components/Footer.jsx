2141e686b38a0ad7d87a658cd41710f69fd40d8d
Duhan Ozarslan <r.d.ozarslan@gmail.com>
2023-11-05 13:57:57 +0300

    footer styles changed

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 
 export default function Footer() {
   return (
-    <div className="relative h-10 bottom-0">
-      <footer className="bg-[#252525]  absolute flex items-center justify-center text-sm text-white left-0 right-0 bottom-0 top-0">
+    <div className="absolute bottom-0 h-10 px-4  text-sm text-white right-0 left-0 ">
+      <footer className=" bg-[#252525] w-full h-full flex items-center justify-center">
         <p>Ⓒ 2022 #VANLIFE</p>
       </footer>
     </div>
