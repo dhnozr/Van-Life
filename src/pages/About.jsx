664abc090ab71138ac7faa856502c6ac11ac153b
Duhan Ozarslan <r.d.ozarslan@gmail.com>
2023-11-05 18:04:10 +0300

    about page styles added

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,5 +1,39 @@
 import React from 'react';
 
 export default function About() {
-  return <h1>About</h1>;
+  return (
+    <>
+      <div>
+        <div>
+          <img
+            className="w-full object-cover h-[400px]"
+            src="/src/images/image55.jpg"
+            alt=""
+          />
+        </div>
+        <div className="text-[#161616] grid gap-4 my-4">
+          <h1 className="text-4xl font-bold">
+            Don’t squeeze in a sedan when you could relax in a van.
+          </h1>
+          <p>
+            Our mission is to enliven your road trip with the perfect travel van
+            rental. Our vans are recertified before each trip to ensure your
+            travel plans can go off without a hitch. (Hitch costs extra 😉)
+          </p>
+          <p>
+            Our team is full of vanlife enthusiasts who know firsthand the magic
+            of touring the world on 4 wheels.
+          </p>
+        </div>
+        <div className="my-5 bg-[#FFCC8D] p-8">
+          <h2 className="text-2xl font-bold mb-6">
+            Your destination is waiting. <br /> Your van is ready.
+          </h2>
+          <button className="bg-black py-3 px-10 text-white rounded-xl">
+            Explore our vans
+          </button>
+        </div>
+      </div>
+    </>
+  );
 }
