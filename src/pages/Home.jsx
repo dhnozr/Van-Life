476dd65a2d5f4683325e5907bafaffb41aeff864
Duhan Ozarslan <r.d.ozarslan@gmail.com>
2023-11-05 13:39:23 +0300

    home page styles added

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,23 @@
 import React from 'react';
+import heroBanner from '../images/image54.jpg';
 
 export default function Home() {
-  return <h1>Home</h1>;
+  return (
+    <>
+      <div className="bg-hero-banner h-screen max-h-[calc(100vh-104px)] bg-no-repeat bg-center bg-cover">
+        <div className="flex flex-col items-center justify-center gap-10 text-white">
+          <h1 className="text-4xl">
+            You got the travel plans, we got the travel vans.
+          </h1>
+          <h2>
+            Add adventure to your life by joining the #vanlife movement. Rent
+            the perfect van to make your perfect road tri
+          </h2>
+          <button className="bg-[#FF8C38] py-3 px-8  rounded-md">
+            Find your van
+          </button>
+        </div>
+      </div>
+    </>
+  );
 }
