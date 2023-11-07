450f447c98a606aea0cec4ad008c18a69b59c847
Duhan Ozarslan <r.d.ozarslan@gmail.com>
2023-11-07 20:50:07 +0300

    page created

---
+++ src/components/Details.jsx
@@ -0,0 +1,12 @@
+import React, { useContext } from 'react';
+
+export default function Details() {
+  return (
+    <div>
+      <div>
+        <h4>Name:</h4>
+        <span>Explorer</span>
+      </div>
+    </div>
+  );
+}
