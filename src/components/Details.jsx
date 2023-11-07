c452896cc08d3815fab40b2a6dc9e3c2d664d52a
Duhan Ozarslan <r.d.ozarslan@gmail.com>
2023-11-07 22:32:31 +0300

    route added

--- src/components/Details.jsx
+++ src/components/Details.jsx
@@ -1,11 +1,18 @@
 import React, { useContext } from 'react';
+import { useOutletContext } from 'react-router-dom';
 
 export default function Details() {
+  const data = useOutletContext();
+  console.log(data);
   return (
     <div>
-      <div>
+      <div className="flex gap-2">
         <h4>Name:</h4>
-        <span>Explorer</span>
+        <span>{data.name}</span>
+      </div>
+      <div className="flex gap-2">
+        <h4>Description:</h4>
+        <span>{data.description}</span>
       </div>
     </div>
   );
