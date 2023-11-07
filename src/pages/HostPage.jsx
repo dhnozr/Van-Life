14cb06d6e8752b1c6967b6c38d158d5608385cdb
Duhan Ozarslan <r.d.ozarslan@gmail.com>
2023-11-07 20:49:28 +0300

    navlinks added

--- src/pages/HostPage.jsx
+++ src/pages/HostPage.jsx
@@ -1,3 +1,4 @@
+import { Underline } from 'lucide-react';
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 import { Outlet } from 'react-router-dom';
@@ -8,16 +9,45 @@ export default function HostPage() {
       <nav className="bg-[#FFF7ED]">
         <ul className="flex items-center text-sm justify-between px-4 py-4  md:text-lg md:justify-normal md:gap-4 ">
           <li>
-            <NavLink to={`.`}>Dashboard</NavLink>
+            <NavLink
+              className={({ isActive }) =>
+                isActive ? 'underline underline-offset-2 font-bold' : null
+              }
+              to={`.`}
+              end
+            >
+              Dashboard
+            </NavLink>
           </li>
           <li>
-            <NavLink to={`/host/income`}>Income</NavLink>
+            <NavLink
+              className={({ isActive }) =>
+                isActive ? 'underline underline-offset-2 font-bold' : null
+              }
+              to={`/host/income`}
+            >
+              Income
+            </NavLink>
           </li>
           <li>
-            <NavLink to={`/host/vans`}>Vans</NavLink>
+            <NavLink
+              className={({ isActive }) =>
+                isActive ? 'underline underline-offset-2 font-bold' : null
+              }
+              to={`/host/vans`}
+            >
+              Vans
+            </NavLink>
           </li>
           <li>
-            <NavLink to={`/host/reviews`}>Reviews</NavLink>
+            <NavLink
+              className={({ isActive }) =>
+                isActive ? 'underline underline-offset-2 font-bold' : null
+              }
+              to={`/host/reviews`}
+            >
+              Reviews
+            </NavLink>
           </li>
         </ul>
       </nav>
