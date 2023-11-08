53280746313b07e6889cab91e73971f8c80a7163
Duhan Ozarslan <r.d.ozarslan@gmail.com>
2023-11-08 22:45:25 +0300

    price data added

--- src/components/Pricing.jsx
+++ src/components/Pricing.jsx
@@ -1,5 +1,12 @@
 import React from 'react';
+import { useOutletContext } from 'react-router-dom';
 
 export default function Pricing() {
-  return <div className="text-2xl">Pricing</div>;
+  const data = useOutletContext();
+  return (
+    <div className="flex gap-2 items-center">
+      <h4 className="font-bold">Pricing:</h4>
+      <span>${data.price}</span>
+    </div>
+  );
 }
