3f9cdb3f2baca530a2fdda7c063107a555be0dec
Duhan Ozarslan <r.d.ozarslan@gmail.com>
2023-11-10 23:15:43 +0300

    Update App.jsx
    loaders added

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,20 @@
 import { useState } from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import {
+  BrowserRouter,
+  RouterProvider,
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  Routes,
+} from 'react-router-dom';
 import About from './pages/About';
 import Layout from './Layout';
 import Home from './pages/Home';
-import Vans from './pages/Vans';
-import VanDetailPage from './pages/VanDetailPage';
+import Vans, { loader } from './pages/Vans';
+import VanDetailPage, {
+  loader as VanDetailPageLoader,
+} from './pages/VanDetailPage';
 import HostPage from './pages/HostPage';
 import Dashboard from './components/Dashboard';
 import Income from './components/Income';
@@ -16,40 +25,89 @@ import Details from './components/Details';
 import Pricing from './components/Pricing';
 import Photos from './components/Photos';
 import NotFoundPage from './components/404Page';
+import Login from './pages/Login';
 
 import './App.css';
 
-function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<Home />} />
-          <Route path="about" element={<About />} />
-          <Route path="vans">
-            <Route index element={<Vans />} />
-            <Route path=":id" element={<VanDetailPage />} />
-          </Route>
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<Layout />}>
+      <Route index element={<Home />} />
+      <Route path="about" element={<About />} />
+      <Route path="login" element={<Login />} />
 
-          <Route path="host" element={<HostPage />}>
-            <Route index element={<Dashboard />} />
-            <Route path="income" element={<Income />} />
+      <Route path="vans">
+        <Route index element={<Vans />} loader={loader} />
+        <Route
+          path=":id"
+          loader={VanDetailPageLoader}
+          element={<VanDetailPage />}
+        />
+      </Route>
 
-            <Route path="vans">
-              <Route index element={<HostVans />} />
-              <Route path=":id" element={<HostVansDetail />}>
-                <Route index element={<Details />} />
-                <Route path="pricing" element={<Pricing />} />
-                <Route path="photos" element={<Photos />} />
-              </Route>
-            </Route>
-            <Route path="reviews" element={<Reviews />} />
+      <Route path="host" element={<HostPage />}>
+        <Route
+          loader={async () => {
+            return null;
+          }}
+          index
+          element={<Dashboard />}
+        />
+        <Route
+          loader={async () => {
+            return null;
+          }}
+          path="income"
+          element={<Income />}
+        />
+
+        <Route path="vans">
+          <Route
+            loader={async () => {
+              return null;
+            }}
+            index
+            element={<HostVans />}
+          />
+          <Route
+            loader={async () => {
+              return null;
+            }}
+            path=":id"
+            element={<HostVansDetail />}
+          >
+            <Route
+              loader={async () => {
+                return null;
+              }}
+              index
+              element={<Details />}
+            />
+            <Route
+              loader={async () => {
+                return null;
+              }}
+              path="pricing"
+              element={<Pricing />}
+            />
+            <Route
+              loader={async () => {
+                return null;
+              }}
+              path="photos"
+              element={<Photos />}
+            />
           </Route>
-          <Route path="*" element={<NotFoundPage />} />
         </Route>
-      </Routes>
-    </BrowserRouter>
-  );
+        <Route path="reviews" element={<Reviews />} />
+      </Route>
+      <Route path="*" element={<NotFoundPage />} />
+    </Route>
+  )
+);
+
+function App() {
+  return <RouterProvider router={router} />;
 }
 
 export default App;
