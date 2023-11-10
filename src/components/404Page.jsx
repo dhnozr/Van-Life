1169099fee1130cadd9b2a9275effe72517b9e54
Duhan Ozarslan <r.d.ozarslan@gmail.com>
2023-11-10 17:11:32 +0300

    Update 404Page.jsx
    styles added

--- src/components/404Page.jsx
+++ src/components/404Page.jsx
@@ -3,10 +3,14 @@ import { Link } from 'react-router-dom';
 
 export default function NotFoundPage() {
   return (
-    <div>
-      <h1>Sorry, the page you were looking for was not found.</h1>
+    <div className="flex flex-col items-center justify-center gap-3 min-h-[calc(100vh-104px)]">
+      <h1 className="text-xl md:text-2xl">
+        Sorry, the page you were looking for was not found.
+      </h1>
       <Link to={`/`}>
-        <button>Return Home</button>
+        <button className="bg-black text-white px-6 py-1 text-2xl rounded-md">
+          Return Home
+        </button>
       </Link>
     </div>
   );
